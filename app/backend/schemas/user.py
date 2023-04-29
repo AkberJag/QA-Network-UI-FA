faa1c0251315f36ae12fb8e855e5af0047c448a0
unknown <akberjag@gmail.com>
2023-04-29 14:05:50 +0530

    🎉Created user schema

---
+++ app/backend/schemas/user.py
@@ -0,0 +1,32 @@
+"""User pydantic schema"""
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    """shared properties"""
+
+    email: EmailStr | None = None
+
+
+class UserCreate(UserBase):
+    """Properties to receive via API on creation"""
+
+    email: EmailStr | None = None
+    password: str
+
+
+class UserUpdate(UserBase):
+    """Properties to receive via API on update"""
+
+    password: str
+
+
+class UserInDBBase(UserBase):
+    id: int | None = None
+
+
+class UserInDB(UserInDBBase):
+    """Additional properties stored in DB"""
+
+    hashed_password: str
