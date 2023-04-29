5d0715ddb5b7bc279bbe615fd47d34310df9774a
unknown <akberjag@gmail.com>
2023-04-29 15:53:48 +0530

    🔧  Updated db folder name in config

--- app/backend/core/config.py
+++ app/backend/core/config.py
@@ -15,10 +15,10 @@ class Settings(BaseSettings):
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
     # create the db folder to avoid the error > sqlalchemy.exc.OperationalError: (sqlite3.OperationalError) unable to open database file
-    if not os.path.exists(os.path.join(BASE_DIR, "db")):
-        os.makedirs(os.path.join(BASE_DIR, "db"))
+    if not os.path.exists(os.path.join(BASE_DIR, "dbs")):
+        os.makedirs(os.path.join(BASE_DIR, "dbs"))
 
-    DB_FOLDER = os.path.join(BASE_DIR, "db")
+    DB_FOLDER = os.path.join(BASE_DIR, "dbs")
     DB_NAME = "dev.sqlite"
     DB_PATH = os.path.join(DB_FOLDER, DB_NAME)
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}"
