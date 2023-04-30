2765d4057c0b81e8d2734b4da289f1624176dc0f
unknown <akberjag@gmail.com>
2023-05-01 00:54:50 +0530

    🚚  added apiv1 url
    curd to crud

--- app/backend/api/api_v1/api.py
+++ app/backend/api/api_v1/api.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from app.backend.api.api_v1.endpoints import login
+from app.backend.api.api_v1.endpoints import login, users
+from app.backend.core.config import settings
 
-api_v1_router = APIRouter()
+api_v1_router = APIRouter(prefix=settings.API_V1_URL)
 api_v1_router.include_router(login.router, tags=["login"])
+api_v1_router.include_router(users.router, prefix="/users", tags=["users"])
