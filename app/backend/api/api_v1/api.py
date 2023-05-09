dd2062a2a15ebf95707458784b550d52189bf8fb
unknown <akberjag@gmail.com>
2023-05-10 02:19:54 +0530

    ✨ added network template to the apiv1

--- app/backend/api/api_v1/api.py
+++ app/backend/api/api_v1/api.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
-from app.backend.api.api_v1.endpoints import login, users
+from app.backend.api.api_v1.endpoints import login, users, network_template
 from app.backend.core.config import settings
 
 api_v1_router = APIRouter(prefix=settings.API_V1_URL)
 api_v1_router.include_router(login.router, tags=["login"])
 api_v1_router.include_router(users.router, prefix="/users", tags=["users"])
+api_v1_router.include_router(
+    network_template.router, prefix="/network_template", tags=["network_template"]
+)
