f73027e285b052767c39499e2f13b4be69093f35
unknown <akberjag@gmail.com>
2023-05-11 00:44:24 +0530

    🏷️ Updated 'update' operation logic in crud base

--- app/backend/crud/base.py
+++ app/backend/crud/base.py
@@ -38,12 +38,22 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchematype]):
         """Retrieves an object from the database by its ID"""
         return db.query(self.model).filter(self.model.id == id).first()
 
-    def update(self, db: Session, obj_in: UpdateSchematype):
+    def update(self, db: Session, obj_in: UpdateSchematype, db_obj: ModelType):
         """Updates an existing object in the database"""
-        db.add(obj_in)
+        obj_in_data = obj_in.dict()
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+
+        for field in obj_in_data:
+            if field in update_data:
+                setattr(db_obj, field, update_data[field])
+
+        db.add(db_obj)
         db.commit()
-        db.refresh(obj_in)
-        return obj_in
+        db.refresh(db_obj)
+        return db_obj
 
     def delete(self, db: Session, id: int):
         """Delete an object from the db"""
