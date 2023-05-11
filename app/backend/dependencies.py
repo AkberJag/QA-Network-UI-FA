b7486ff09f685c8ee30d6b70a10a6b0dac05ad8e
Akber-MAC <akberjag@gmail.com>
2023-05-11 12:30:06 +0530

    ✨ added get current user dependency

--- app/backend/dependencies.py
+++ app/backend/dependencies.py
@@ -1,10 +1,17 @@
 from typing import Generator
 
+from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from pydantic import ValidationError
 
 from app.backend.core.database import SessionLocal
 from app.backend.core.config import settings
+from app.backend.core import security
+from app.backend.models import User
+from app.backend import schemas
+from app.backend.crud import crud_user
 
 oauth2_barer = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_URL}/login/token")
 
@@ -16,3 +23,28 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
+
+
+def get_current_user(
+    db: Session = Depends(get_db), token: str = Depends(oauth2_barer)
+) -> User:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = schemas.Token(**payload)
+    except JWTError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        ) from exc
+    except ValidationError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Invalid input",
+        ) from exc
+
+    user = crud_user.get(db, token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
