0e97ec948fed677e1fdfe9c8ced0c4c542dd55e1
unknown <akberjag@gmail.com>
2023-05-14 15:31:39 +0530

    ✨ Added ip address endpoint to the api v1

--- app/backend/api/api_v1/api.py
+++ app/backend/api/api_v1/api.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
-from app.backend.api.api_v1.endpoints import login, users, network_template
+from app.backend.api.api_v1.endpoints import login, users, network_template, ip_address
 from app.backend.core.config import settings
 
 api_v1_router = APIRouter(prefix=settings.API_V1_URL)
-api_v1_router.include_router(login.router, tags=["login"])
-api_v1_router.include_router(users.router, prefix="/users", tags=["users"])
+api_v1_router.include_router(login.router, tags=["Login"])
+api_v1_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_v1_router.include_router(
-    network_template.router, prefix="/network_template", tags=["network_template"]
+    network_template.router, prefix="/network_template", tags=["Network Template"]
+)
+
+api_v1_router.include_router(
+    ip_address.router, prefix="/ip_address", tags=["Ip Address"]
 )
