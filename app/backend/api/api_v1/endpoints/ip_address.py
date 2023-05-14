a293cd8926964b6656609e4f0b89975eaa38453f
unknown <akberjag@gmail.com>
2023-05-14 23:10:43 +0530

    ✨Added 'return all Ip address' fn

--- app/backend/api/api_v1/endpoints/ip_address.py
+++ app/backend/api/api_v1/endpoints/ip_address.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.backend.dependencies import get_db
-from app.backend.crud import crud_ip_address
+from app.backend.crud import crud_ip_address, crud_network_template
 from app.backend.models.ip_address import IPAddress
 from app.backend.schemas.ip_address import IPAddressOut, IPAddressCreate, IPAddressInDB
 
@@ -11,12 +11,20 @@ router = APIRouter()
 
 @router.get("/", response_model=list[IPAddressOut])
 async def get_ip_address(db: Session = Depends(get_db)) -> list[IPAddressOut]:
+    """Return all ip address"""
     return db.query(IPAddress).all()
 
 
 @router.post("/", response_model=IPAddressOut)
 async def create_new_ip(ip_address_in: IPAddressCreate, db: Session = Depends(get_db)):
     """Create new IP address"""
+
+    network_template = crud_network_template.get(db, ip_address_in.network_template_id)
+    if not network_template:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "Selected owner does not exist")
+
+    print(network_template.no_of_pcs)
+
     ip_address = crud_ip_address.get_by_pc_name(db, ip_address_in.pc_name)
     if ip_address:
         raise HTTPException(
@@ -27,7 +35,7 @@ async def create_new_ip(ip_address_in: IPAddressCreate, db: Session = Depends(ge
     if ip_address:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="This ip address already in added",
+            detail="This ip address already added",
         )
 
     obj_in = IPAddressInDB(**ip_address_in.dict())
