1f979f13397f76996a24f55f381cab5bce1f07a5
unknown <akberjag@gmail.com>
2023-05-14 15:34:21 +0530

    ✨ Added ip address crud to the crud module

--- app/backend/crud/__init__.py
+++ app/backend/crud/__init__.py
@@ -1,2 +1,3 @@
-from app.backend.crud.crud_user import crud_user
+from .crud_user import crud_user
 from .crud_network_template import crud_network_template
+from .crud_ip_address import crud_ip_address
