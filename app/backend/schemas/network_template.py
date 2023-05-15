1499538e3d46fd5b6585dd9178e9893572b0faa7
unknown <akberjag@gmail.com>
2023-05-16 01:34:31 +0530

    ✨Changed template schema to accept cidr ip and prefix and custom validation for both fields

--- app/backend/schemas/network_template.py
+++ app/backend/schemas/network_template.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, Field
+from app.backend import utils
+from fastapi import HTTPException, status
 
 
 class NetworkTemplateBase(BaseModel):
@@ -6,7 +8,8 @@ class NetworkTemplateBase(BaseModel):
 
     network_template_name: str
 
-    cidr_notation: str
+    cidr_ip: str
+    cidr_suffix: int = Field(ge=1, le=32)
 
     bandwidth_restriction_upload: float
     bandwidth_restriction_download: float
@@ -14,6 +17,18 @@ class NetworkTemplateBase(BaseModel):
     dns_latency: float
     general_latency: float
     packet_loss: float
+
+    @validator("cidr_ip")
+    @classmethod
+    def validate_ip_cidr_notation(cls, v: str) -> str | None:
+        """Custom validation to make sure cidr text is in proper format
+        eg: 1.1.1.1/31
+        """
+        if utils.validate_ip_address_string(v):
+            return v
+        raise HTTPException(
+            status.HTTP_422_UNPROCESSABLE_ENTITY, f"'{v}' is not a valid ip address"
+        )
 
 
 class NetworkTemplateCreate(NetworkTemplateBase):
