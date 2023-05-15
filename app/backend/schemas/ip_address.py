0885a6088d9629cc1df37925a6f48a3e84f43135
unknown <akberjag@gmail.com>
2023-05-16 01:20:25 +0530

    ♻️ Removed unused imports

--- app/backend/schemas/ip_address.py
+++ app/backend/schemas/ip_address.py
@@ -4,8 +4,6 @@ from fastapi import Depends, HTTPException, status
 from pydantic import BaseModel, validator
 from sqlalchemy.orm import Session
 
-from app.backend.dependencies import get_db
-from app.backend.models import NetworkTemplate
 from app.backend import utils
 
 
