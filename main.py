a91ffc89ad8e45eefa108a079f9fdfefc41d7c2f
unknown <akberjag@gmail.com>
2023-05-17 01:01:38 +0530

    ✨ Added the html router module

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 """Main application package containing the app factory function."""
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.backend.api.api_v1 import api
+from app.backend import routers
 
 
 def create_application() -> FastAPI:
@@ -15,6 +17,8 @@ def create_application() -> FastAPI:
 def include_routers(app: FastAPI):
     """Include FastAPI routers."""
     app.include_router(api.api_v1_router)
+    app.include_router(routers.frontend_routers)
 
 
 app = create_application()
+app.mount("/static", StaticFiles(directory="app/frontend/static/"), name="static")
