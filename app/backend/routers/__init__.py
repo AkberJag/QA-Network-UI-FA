6176254ee6aa8c777e7b1da5cafbff29971a90c3
Akber-MAC <akberjag@gmail.com>
2023-05-17 12:39:38 +0530

    🚩 Removed front-end endpoints from openapi schema

--- app/backend/routers/__init__.py
+++ app/backend/routers/__init__.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
+from app.backend.core.config import settings
+
 from . import auth
 
-frontend_routers = APIRouter()
+frontend_routers = APIRouter(
+    include_in_schema=settings.INCLUDE_FRONTEND_ENDPOINTS_IN_OpenAPI_SCHEMA
+)
 frontend_routers.include_router(auth.router)
