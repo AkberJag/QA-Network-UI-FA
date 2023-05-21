383fdd076fd56a098aea2de15f86521bf082a780
unknown <akberjag@gmail.com>
2023-05-21 13:31:31 +0530

    ✨ Added public route to the frontend router

--- app/backend/routers/__init__.py
+++ app/backend/routers/__init__.py
@@ -3,8 +3,11 @@ from fastapi import APIRouter
 from app.backend.core.config import settings
 
 from . import auth
+from . import public
 
 frontend_routers = APIRouter(
     include_in_schema=settings.INCLUDE_FRONTEND_ENDPOINTS_IN_OpenAPI_SCHEMA
 )
+
 frontend_routers.include_router(auth.router)
+frontend_routers.include_router(public.router)
